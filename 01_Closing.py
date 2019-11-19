21bff10063c6772535ab590a100cf4899c84a696
MuellerLouis <muellerlouis@outlook.com>
2019-11-19 09:39:34 +0100

    Created class Research to combine subclasses

--- 01_Closing.py
+++ 01_Closing.py
@@ -10,27 +10,36 @@ pd.set_option("display.max_columns", 8)
 file_snapshots = os.getcwd() + "\\Data\\orders_close_closing_main.csv"
 file_prices = os.getcwd() + "\\Data\\orders_closing_prices.csv"
 
+mode = 'Discovery'
+granularity = 'fine'
+
+if mode == 'Sensitivity':
+	Sens = SensitivityAnalysis(file_snapshots)
+	if granularity == 'rough':
+		percent = np.arange(0.05, 0.55, 0.05)
+	elif granularity == 'fine':
+		percent = np.arange(0.01, 0.51, 0.01)
+	Sens.sens_analysis(key='bid_limit', percents=percent)
+	Sens.sens_analysis(key='ask_limit', percents=percent)
+	Sens.sens_analysis(key='all_limit', percents=percent)
+	Sens.sens_analysis(key='all_market')
+	Sens.export_results('Sensitivity_{}_v1'.format(granularity), 'csv')
+	Sens.export_results('Sensitivity_{}_v1'.format(granularity), 'xlsx')
+	print("<<< Sensitivity Analysis complete >>>")
+
+
+elif mode == 'Discovery':
+	Discovery = PriceDiscovery(file_snapshots, file_prices)
+	Discovery.get_SB()
+	Discovery.discovery_analysis()
+	df = Discovery.results_to_df()
+	Discovery.export_results('Price_Discovery_v1', 'csv')
+	Discovery.export_results('Price_Discovery_v1', 'xlsx')
+	print("<<< Price Discovery complete >>>")
+
+
 #%%
 
-Discovery = PriceDiscovery(file_snapshots, file_prices)
-Discovery.discovery_analysis()
-df = Discovery.results_to_df()
-Discovery.export_results('Price_Discovery_v1', 'csv')
-Discovery.export_results('Price_Discovery_v1', 'xlsx')
-
-
-#%%
-
-Sens = SensitivityAnalysis(file_snapshots)
-
-percent = np.arange(0.05, 0.55, 0.05)
-Sens.sens_analysis(key='bid_limit', percents=percent)
-Sens.sens_analysis(key='ask_limit', percents=percent)
-Sens.sens_analysis(key='all_limit', percents=percent)
-Sens.sens_analysis(key='all_market')
-Sens.export_results('Sensitivity_v1', 'csv')
-Sens.export_results('Sensitivity_v1', 'xlsx')
-print("finished")
 
 #%%
 
