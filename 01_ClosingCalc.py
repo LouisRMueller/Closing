02895ebcb52a97a7ee6f1038cbb0abc1696498e6
MuellerLouis <muellerlouis@outlook.com>
2020-02-13 14:58:14 +0100

    Latest update.

--- 01_ClosingCalc.py
+++ 01_ClosingCalc.py
@@ -4,8 +4,7 @@ pd.set_option('display.width', 180)
 pd.set_option("display.max_columns", 8)
 
 ########################################################################
-file_snapshots = os.getcwd() + "\\Data\\20193115_closing_orders.csv"
-# file_snapshots = os.getcwd() + "\\Data\\orders_close_closing_main_v2.csv"
+file_snapshots = os.getcwd() + "\\Data\\orders_close_closing_main_v3.csv"
 file_prices = os.getcwd() + "\\Data\\orders_closing_prices.csv"
 mode = 'Sensitivity'
 granularity = 'rough'
@@ -15,9 +14,9 @@ granularity = 'rough'
 if mode == 'Sensitivity':
 	Sens = SensitivityAnalysis(file_snapshots)
 	if granularity == 'rough':
-		percent = np.arange(0, 0.55, 0.05)
+		percent = np.arange(0, 0.3, 0.05).round(2)
 	elif granularity == 'fine':
-		percent = np.arange(0, 0.51, 0.01)
+		percent = np.arange(0, 0.51, 0.01).round(2)
 	else:
 		raise ValueError("Wrong input for granularity.")
 		
@@ -25,14 +24,17 @@ if mode == 'Sensitivity':
 	Sens.sens_analysis(key='ask_limit', percents=percent)
 	Sens.sens_analysis(key='all_limit', percents=percent)
 	Sens.sens_analysis(key='all_market')
-	Sens.export_results('Sensitivity_{}_v2'.format(granularity), 'csv')
+	Sens.sens_analysis(key='cont_market')
+
+	Sens.export_results('Sensitivity_{}_v3'.format(granularity), 'csv')
 	print("<<< Sensitivity Sens complete >>>")
 
  
 elif mode == 'Discovery':
 	Discovery = PriceDiscovery(file_snapshots, file_prices)
 	Discovery.discovery_analysis()
-	Discovery.export_results('Price_Discovery_v2', 'csv')
+	Discovery.export_results('Price_Discovery_v3', 'csv')
 	print("<<< Price Disc complete >>>")
 
-
+elif mode == 'Intervals':
+	pass
--- cls_ClosingCalc.py
+++ cls_ClosingCalc.py
@@ -14,11 +14,11 @@ class Research:
 		self._dates = self._snapbook['onbook_date'].unique()
 		self._snapbook.set_index(['onbook_date', 'symbol', 'price'], drop=True, inplace=True)
 		self._snapbook.sort_index(inplace=True)
-		
+
 		self._result_dict = {}  # Collects all the results
-		
+
 		print(">>> Class initiated ({} seconds)".format(round(time() - t0, 2)))
-		
+
 	@staticmethod
 	def _extract_market_orders(imp_df):
 		"""
@@ -37,14 +37,14 @@ class Research:
 		except KeyError:
 			imp_df.loc[0, :] = 0
 			mark_sell = imp_df.loc[0, 'end_close_vol_ask']
-		
+
 		df = imp_df.drop(0, axis=0).sort_index()
-		
+
 		return df, mark_buy, mark_sell
-	
+
 	def get_SB(self):
 		return self._snapbook
-	
+
 	def export_results(self, filename, filetype):
 		df = self.results_to_df()
 		if filetype == 'xlsx':
@@ -61,32 +61,28 @@ class SensitivityAnalysis(Research):
 		:return: dict()
 		"""
 		df, mark_buy, mark_sell = Research._extract_market_orders(imp_df)
-		
+
 		if 0 in df[['end_close_vol_ask', 'end_close_vol_bid']].sum().values:  # Where one side is empty
 			return dict(price=np.nan, imbalance=np.nan, trade_vol=np.nan)
-		
+
 		else:
 			n_lim = df.shape[0]
 			limit_bids, limit_asks = deque(df['end_close_vol_bid'], n_lim), deque(df['end_close_vol_ask'], n_lim)
-			
+
 			neg_bids = limit_bids.copy()
 			neg_bids.appendleft(0)
-			
+
 			cum_bids = mark_buy + sum(limit_bids) - np.cumsum(neg_bids)
 			cum_asks = mark_sell + np.cumsum(limit_asks)
-			
+
 			OIB = cum_bids - cum_asks
 			i = np.argmin(abs(OIB))
 			trade_vol = min(cum_bids[i], cum_asks[i])
 
-			dump_df = pd.DataFrame({'price': df.index, 'cum. bids': cum_bids,
-							    'cum. asks': cum_asks, 'OIB': OIB, 'vol': trade_vol})
-			dump_df.set_index('price', inplace=True)
-
-			output = dict(price=df.index[i], imbalance=OIB[i], trade_vol=trade_vol, dump=dump_df)
+			output = dict(price=df.index[i], imbalance=OIB[i], trade_vol=trade_vol)
 
 			return output
-	
+
 	def _remove_liq(self, date, title, percentage=0, market=None, side=None):
 		"""
 		This function removes a certain percentage of liquidity from the closing auction.
@@ -100,13 +96,13 @@ class SensitivityAnalysis(Research):
 		"""
 		imp_df = copy.deepcopy(self._snapbook.loc[(date, title), :])
 		imp_df.replace({np.nan: 0}, inplace=True)
-		
+
 		if percentage == 0:
-			return imp_df[['end_close_vol_bid', 'end_close_vol_ask']]
-		
+			return imp_df[['end_close_vol_ask', 'end_close_vol_bid']]
+
 		bids = imp_df['end_close_vol_bid'].tolist()
 		asks = imp_df['end_close_vol_ask'].tolist()
-		
+
 		if market == "remove_all":  # Removes all market orders in closing auction
 			ret_df = imp_df.loc[:, ('end_close_vol_ask', 'end_close_vol_bid')]
 			try:
@@ -114,7 +110,17 @@ class SensitivityAnalysis(Research):
 				return ret_df
 			except KeyError:
 				return ret_df
-		
+		elif market == 'remove_cont':
+			ret_df = imp_df.loc[:, ('end_close_vol_ask', 'end_close_vol_bid')]
+			try:
+				ret_df.loc[0, 'end_close_vol_ask'] -= imp_df.loc[0, 'start_close_vol_ask']
+				ret_df.loc[0, 'end_close_vol_bid'] -= imp_df.loc[0, 'start_close_vol_bid']
+				ret_df[ret_df < 0] = 0
+				print(ret_df)
+				return ret_df
+			except KeyError:
+				return ret_df
+
 		else:  # Only considering limit orders for adjustments
 			removable_bid = sum(bids[1:]) * percentage
 			removable_ask = sum(asks[1:]) * percentage
@@ -128,7 +134,7 @@ class SensitivityAnalysis(Research):
 				bids[b] = local_vol - min(local_vol, removable_bid)
 				removable_bid -= min(removable_bid, local_vol)
 				b -= 1
-		
+
 		if side in ['ask', 'all']:
 			a = 1
 			# remaining_liq = removable_ask / percentage
@@ -137,10 +143,10 @@ class SensitivityAnalysis(Research):
 				asks[a] = local_vol - min(local_vol, removable_ask)
 				removable_ask -= min(removable_ask, local_vol)
 				a += 1
-		
+
 		ret_df = pd.DataFrame([asks, bids], index=['end_close_vol_ask', 'end_close_vol_bid'], columns=imp_df.index).T
 		return ret_df
-	
+
 	def sens_analysis(self, key, percents=tuple([1])):
 		"""
 		This function is supposed to exeucte the required calculations and add it to an appropriate data format.
@@ -148,8 +154,6 @@ class SensitivityAnalysis(Research):
 		:param key: String with mode of calculation ['bid_limit','ask_limit','all_limit','all_market','cont_market']
 		:param percents: Iterable object with integers of percentages to be looped through.
 		"""
-		dump = {}
-		
 		if key == 'bid_limit':
 			side, mkt = 'bid', None
 		elif key == 'ask_limit':
@@ -158,42 +162,42 @@ class SensitivityAnalysis(Research):
 			side, mkt = 'all', None
 		elif key == 'all_market':
 			side, mkt = 'all', 'remove_all'
-		# elif key == "cont_market":
-		# 	side, mkt = 'all', 'remove_cont'
-		
+		elif key == "cont_market":
+			side, mkt = 'all', 'remove_cont'
+
 		else:
-			raise ValueError("key input not in ['bid_limit','ask_limit','all_limit','all_market','end_cont_market']")
-		
+			raise ValueError("key input not in ['bid_limit','ask_limit','all_limit','all_market','cont_market']")
+
 		for date in self._dates:
 			t0 = time()
 			current_symbols = self.get_SB().loc[date, :].index.get_level_values(0).unique()
-			dump.update({date:{}})
-			
+
+			# for symbol in ['UBSG']:
 			for symbol in current_symbols:
 				close_df = self._remove_liq(date=date, title=symbol, percentage=0)
 				close_uncross = self._calc_uncross(close_df)
-				dump[date].update({symbol: {}})
 
 				for p in percents:
 					res = self._result_dict[key, date, symbol, p] = {}
-					
+
 					remove_df = self._remove_liq(date=date, title=symbol, percentage=p, side=side, market=mkt)
 					remove_uncross = self._calc_uncross(remove_df)
 
-					dump[date][symbol].update({p: remove_uncross.pop('dump', None)})
-					
 					res['close_price'] = close_uncross['price']
 					res['close_vol'] = close_uncross['trade_vol']
 					res['close_imbalance'] = close_uncross['imbalance']
-					
+
 					res['adj_price'] = remove_uncross['price']
 					res['adj_vol'] = remove_uncross['trade_vol']
 					res['adj_imbalance'] = remove_uncross['imbalance']
-			
+
 			print(">> {} finished ({} seconds)".format(date, round(time() - t0, 2)))
 
-		return dump if dump else None
-	
+		return
+
+	def interval_analysis(self):
+		pass
+
 	def results_to_df(self):
 		"""
 		Export such that it can be used further in later stages.
@@ -201,7 +205,7 @@ class SensitivityAnalysis(Research):
 		df = pd.DataFrame.from_dict(self._result_dict, orient='index')
 		df.index.set_names(['Mode', 'Date', 'Symbol', 'Percent'], inplace=True)
 		return df
-	
+
 
 class PriceDiscovery(Research):
 	def __init__(self, file_snapshots, file_close_prices):
@@ -224,25 +228,25 @@ class PriceDiscovery(Research):
 		"""
 		imp_df = copy.deepcopy(self._snapbook.loc[(date, title), :])
 		imp_df.replace({np.nan: 0}, inplace=True)
-		
+
 		df, mark_buy, mark_sell = Research._extract_market_orders(imp_df)
-		
+
 		if 0 in df[['end_close_vol_ask', 'end_close_vol_bid']].sum().values:
 			return dict(price=np.nan, imbalance=np.nan, trade_vol=np.nan)
 
 		else:
 			n_lim = df.shape[0]
 			limit_bids, limit_asks = deque(df['end_close_vol_bid'], n_lim), deque(df['end_close_vol_ask'], n_lim)
-			
+
 			neg_bids = limit_bids.copy()
 			neg_bids.appendleft(0)
-			
+
 			cum_bids = mark_buy + sum(limit_bids) - np.cumsum(neg_bids)
 			cum_asks = mark_sell + np.cumsum(limit_asks)
-			
+
 			OIB = cum_bids - cum_asks
 			i = np.argmin(abs(OIB))
-			
+
 			output = dict(price=df.index[i], imbalance=OIB[i], trade_vol=min(cum_bids[i], cum_asks[i]))
 			return output
 
@@ -260,8 +264,9 @@ class PriceDiscovery(Research):
 			pass
 
 		n_lim = imp_df.shape[0]
-		limit_bids, limit_asks = deque(imp_df['end_cont_vol_bid'], n_lim) , deque(imp_df['end_cont_vol_ask'], n_lim)
-		neg_asks = limit_asks.copy() ; neg_asks.appendleft(0)
+		limit_bids, limit_asks = deque(imp_df['end_cont_vol_bid'], n_lim), deque(imp_df['end_cont_vol_ask'], n_lim)
+		neg_asks = limit_asks.copy();
+		neg_asks.appendleft(0)
 
 		cum_bids = np.cumsum(limit_bids)
 		cum_asks = sum(limit_asks) - np.cumsum(neg_asks)
@@ -325,4 +330,3 @@ class PriceDiscovery(Research):
 		df.index.set_names(['Date', 'Symbol'], inplace=True)
 		df.sort_index()
 		return df
-
